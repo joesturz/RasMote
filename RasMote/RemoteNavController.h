e2caf154274c37c01a86fdc1a03d4902c0d86c79
Joe Sturzenegger <joesturz84@gmail.com>
2015-06-13 17:49:31 -0600

    Added changes to the navigation bar colors and added a class to hold the code for the navigation controllers

---
+++ RasMote/RemoteNavController.h
@@ -0,0 +1,13 @@
+//
+//  RemoteNavController.h
+//  RasMote
+//
+//  Created by Joe Sturzenegger on 6/13/15.
+//  Copyright (c) 2015 Joe Sturzenegger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RemoteNavController : UINavigationController
+
+@end
---
+++ RasMote/RemoteNavController.m
@@ -0,0 +1,23 @@
+//
+//  RemoteNavController.m
+//  RasMote
+//
+//  Created by Joe Sturzenegger on 6/13/15.
+//  Copyright (c) 2015 Joe Sturzenegger. All rights reserved.
+//
+
+#import "RemoteNavController.h"
+
+@implementation RemoteNavController
+
+-(void) viewDidLoad
+{
+    //[self.navigationBar setBackgroundColor:[UIColor colorWithRed:0.0f/255.0f green:0.0f/255.0f blue:0.0f/255.0f alpha:1.0]];
+    float greyValue = 85.0f;
+    [self.navigationBar setBarTintColor:[UIColor colorWithRed:greyValue/255.0f green:greyValue/255.0f blue:greyValue/255.0f alpha:1.0]];
+    [self.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                [UIColor colorWithRed:255.0/255.0 green:161.0/255.0 blue:0.0/255.0 alpha:1.0], NSForegroundColorAttributeName, nil]];
+    [self.navigationBar setAlpha:0.0];
+    //255	161	0
+}
+@end
