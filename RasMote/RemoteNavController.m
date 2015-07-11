bbf72f83bdddda7974fe42b7fc7f0ee708f1c096
Joe Sturzenegger <joesturz84@gmail.com>
2015-07-11 15:31:19 -0600

    added color change and additional setting updates

--- RasMote/RemoteNavController.m
+++ RasMote/RemoteNavController.m
@@ -16,9 +16,12 @@
     float greyValue = 85.0f;
     [self.navigationBar setBarTintColor:[UIColor colorWithRed:greyValue/255.0f green:greyValue/255.0f blue:greyValue/255.0f alpha:1.0]];
     [self.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                [UIColor colorWithRed:255.0/255.0 green:161.0/255.0 blue:0.0/255.0 alpha:1.0], NSForegroundColorAttributeName, nil]];
+                                                [UIColor colorWithRed:212.0/255.0f green:125.0/255.0f blue:29.0/255.0f alpha:1.0], NSForegroundColorAttributeName, nil]];
     [self.navigationBar setAlpha:0.0];
     //255	161	0
+    //162	168	162
+    //230	135	32
+    //212	125	29
 }
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
